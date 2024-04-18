e5dc082960d5e9bed7a4a9384cdb01b0ceaeb384
Sander N <koeqatel@gmail.com>
2024-04-18 17:11:46 +0200

    fixed unzipping and autodelete of the downloaded file, split output in two bitmaps

--- HackThisSite/Level4.Designer.cs
+++ HackThisSite/Level4.Designer.cs
@@ -29,29 +29,45 @@
         private void InitializeComponent()
         {
             this.outputImage = new System.Windows.Forms.PictureBox();
+            this.outputImage2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.outputImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputImage2)).BeginInit();
             this.SuspendLayout();
             // 
             // outputImage
             // 
             this.outputImage.BackColor = System.Drawing.Color.Black;
             this.outputImage.Location = new System.Drawing.Point(0, 0);
+            this.outputImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.outputImage.Name = "outputImage";
-            this.outputImage.Size = new System.Drawing.Size(1000, 1000);
+            this.outputImage.Size = new System.Drawing.Size(750, 812);
             this.outputImage.TabIndex = 0;
             this.outputImage.TabStop = false;
             // 
+            // outputImage2
+            // 
+            this.outputImage2.BackColor = System.Drawing.Color.Black;
+            this.outputImage2.Location = new System.Drawing.Point(750, 0);
+            this.outputImage2.Margin = new System.Windows.Forms.Padding(2);
+            this.outputImage2.Name = "outputImage2";
+            this.outputImage2.Size = new System.Drawing.Size(750, 812);
+            this.outputImage2.TabIndex = 1;
+            this.outputImage2.TabStop = false;
+            // 
             // Level4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 1000);
+            this.ClientSize = new System.Drawing.Size(1500, 812);
+            this.Controls.Add(this.outputImage2);
             this.Controls.Add(this.outputImage);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Level4";
             this.Text = "Level4";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Level4_FormClosed);
             this.Load += new System.EventHandler(this.Level4_Load);
             ((System.ComponentModel.ISupportInitialize)(this.outputImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputImage2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox outputImage;
+        private System.Windows.Forms.PictureBox outputImage2;
     }
 }
--- HackThisSite/Level4.cs
+++ HackThisSite/Level4.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using ICSharpCode.SharpZipLib.BZip2;
 
 namespace HackThisSite
 {
@@ -35,21 +35,35 @@ namespace HackThisSite
         private string unZipFile()
         {
             //TODO: Unzip the file
-            FileInfo zipFileName = new FileInfo();
-            using (FileStream fileToDecompressAsStream = zipFileName.OpenRead())
+            string filePath = "C:\\Users\\Koeqatel\\Downloads\\plotMe.xml.bz2";
+
+            // Read the compressed file directly
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
             {
-                string decompressedFileName = @"c:\bzip2\decompressed.txt";
-                using (FileStream decompressedStream = File.Create(decompressedFileName))
+                // Create a BZip2 input stream
+                BZip2InputStream zisUncompressed = new BZip2InputStream(fs);
+
+                // Create a memory stream to hold the decompressed data
+                MemoryStream msUncompressed = new MemoryStream();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                // Read from the BZip2 stream and write to the memory stream
+                while ((bytesRead = zisUncompressed.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    try
-                    {
-                        BZip2.Decompress(fileToDecompressAsStream, decompressedStream, true);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    msUncompressed.Write(buffer, 0, bytesRead);
                 }
+
+                // Close the streams
+                zisUncompressed.Close();
+                msUncompressed.Close();
+
+                // Convert the decompressed data to a string
+                string sUncompressed = Encoding.ASCII.GetString(msUncompressed.ToArray());
+
+                // Output the decompressed data
+                File.Delete(filePath);
+                return sUncompressed;
             }
         }
         private void Level4_Load(object sender, EventArgs e)
@@ -58,8 +72,12 @@ namespace HackThisSite
             Graphics graphicsOutput = Graphics.FromImage(drawingBitmap);
             outputImage.Image = drawingBitmap;
 
+            Bitmap drawing2Bitmap = new Bitmap(outputImage2.Width, outputImage2.Height);
+            Graphics graphics2Output = Graphics.FromImage(drawing2Bitmap);
+            outputImage2.Image = drawing2Bitmap;
+
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load("Assets/plotMe.xml");
+            xmlDoc.LoadXml(unZipFile());
             XmlElement root = xmlDoc.DocumentElement;
 
             XmlNodeList lines = root.SelectNodes("Line");
@@ -75,7 +93,10 @@ namespace HackThisSite
                 float XEnd = float.Parse(line.SelectNodes("XEnd")[0].InnerText);
                 float YEnd = float.Parse(line.SelectNodes("YEnd")[0].InnerText);
 
-                graphicsOutput.DrawLine(pen, XStart, YStart, XEnd, YEnd);
+                if (color == "blue" || color == "green" || color == "red")
+                    graphicsOutput.DrawLine(pen, XStart, YStart, XEnd, YEnd);
+                else if (color == "yellow" || color == "white")
+                    graphics2Output.DrawLine(pen, XStart, YStart, XEnd, YEnd);
             }
 
             XmlNodeList arcs = root.SelectNodes("Arc");
@@ -92,12 +113,14 @@ namespace HackThisSite
                 float XCenter = float.Parse(arc.SelectNodes("XCenter")[0].InnerText);
                 float Radius = float.Parse(arc.SelectNodes("Radius")[0].InnerText);
 
-                //graphicsOutput.DrawArc(new Pen(Color.FromName("blue")), 50, 50, 100, 100, 180, 100);
-                graphicsOutput.DrawArc(pen, XCenter - Radius, YCenter - Radius, Radius * 2, Radius * 2, ArcStart, ArcExtend);
-                //graphicsOutput.DrawArc(new Pen(Color.FromName("red")), XCenter, YCenter, ArcStart, ArcExtend, Radius, 10);
+                if (color == "blue" || color == "green" || color == "red")
+                    graphicsOutput.DrawArc(pen, XCenter - Radius, YCenter - Radius, Radius * 2, Radius * 2, ArcStart, ArcExtend);
+                else if (color == "yellow" || color == "white")
+                    graphics2Output.DrawArc(pen, XCenter - Radius, YCenter - Radius, Radius * 2, Radius * 2, ArcStart, ArcExtend);
             }
 
             outputImage.Invalidate();
+            outputImage2.Invalidate();
         }
     }
 }
